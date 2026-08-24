370c22ae5a5f730d8c32732c14ca0fe7662d1b35
dawndadawg <shaotru2016@gmail.com>
2026-08-24 16:36:25 +0700

    Added Lab02 header (again)
    the first time I forgot to save the header so it does not really added

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,4 +1,12 @@
-﻿namespace Lab02
+﻿/*
+ * Student ID : 1690701360
+ * Name       : Lab02
+ * Section    : 129B
+ * No.        : N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+namespace Lab02
 {
     internal class Program
     {
