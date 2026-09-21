fe1106097ff55d7c2613a6264108f0714127c9bd
dawndadawg <shaotru2016@gmail.com>
2026-09-21 16:45:10 +0700

    finished Lab06

--- Lab06/Program.cs
+++ Lab06/Program.cs
@@ -12,35 +12,52 @@ namespace Lab06
     {
         static void Main(string[] args)
         {
-            //Random rng = new Random();
-            //int level = rng.Next(1,21);
+            int playerHp = 100;
+            int arachnidHp = 500;
+            int playerAtk = 350;
+            int potBoost = 200;
+            int arachnidAtk = 50;
 
-            Console.WriteLine($"Your level (1-99): ");
-            bool ok = int.TryParse(Console.ReadLine(), out int level);
-            bool hasKey = false;
+            Console.WriteLine("|=====MONSTER HUNTER : HOW WILD=====|");
+            Console.WriteLine($"the mission is to defeat the Arachnid ({arachnidHp} HP)");
 
-            if (!ok || level < 1 || level > 99)
+            Console.WriteLine("Action 1 : Attack");
+            Console.WriteLine("Action 2 : Drink ATK boost potion");
+            Console.WriteLine("Action 3 : Flee");
+
+            Console.WriteLine("Choose your action (1-3): ");
+            bool isInputOk = int.TryParse(Console.ReadLine(), out int action);
+
+            if (isInputOk == false || action < 1 || action > 3)
             {
-                Console.WriteLine("Invalid level.");
+                Console.WriteLine("\nYou stand still. The monster stomp you to death.\nGAME OVER");
             }
-            else if (level >= 10 && hasKey)
+            else if (action == 1)
             {
-                Console.WriteLine("the door opens.\nthe boss awaits.");
+                Console.WriteLine($"\nYou attacked the Arachnid for {playerAtk} damage.");
+                int arachnidHpAfterAttack = arachnidHp - playerAtk;
+                Console.WriteLine($"Arachnid take {playerAtk} damage, now they have {arachnidHpAfterAttack} HP left.");
             }
-            else if (level >= 5)
+            else if (action == 2)
             {
-                if (hasKey)
+                Console.WriteLine("\nYou drank an ATK boost potion.");
+                int playerAtkAfterBoost = playerAtk + potBoost;
+                Console.WriteLine($"Your attack increased by {potBoost} damage. You can now deal {playerAtkAfterBoost} damage.");
+                Console.WriteLine($"The Arachnid attacks you for {arachnidAtk} damage, you now have {playerHp - arachnidAtk} HP left.");
+                Console.WriteLine($"You attacked the Arachnid for {playerAtkAfterBoost} damage.");
+                int arachnidHpAfterAttack = arachnidHp - playerAtkAfterBoost;
+                if (arachnidHpAfterAttack <= 0)
                 {
-                    Console.WriteLine("the door opens.");
+                    Console.WriteLine("The mission is completed, the Arachnid has been defeated.");
                 }
                 else
                 {
-                    Console.WriteLine("locked. find a key.");
+                    Console.WriteLine("continue to next action...");
                 }
             }
-            else
+            else if (action == 3)
             {
-                Console.WriteLine("nothing happened.");
+                Console.WriteLine("\nYou run away and abandon the mission, the guild had mark you as traitor and put the bounty on you.");
             }
         }
     }
