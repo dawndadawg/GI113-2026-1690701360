64c0d907cc9da8b4949401ff520a35c2f97b1964
dawndadawg <shaotru2016@gmail.com>
2026-09-21 14:37:06 +0700

    Added Lab06 Header

---
+++ Lab06/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+*Student ID: 1690701360
+* Name       : Lab06
+* Section    : 129B
+* No.        : N / A
+* Course     : GI113 Computer Programming (GI)
+*/
+using System;
+namespace Lab06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
